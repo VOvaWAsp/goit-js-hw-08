0b8a4eefdf1b15bef173a0b134a255a93a556181
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2023-11-20 22:09:08 +0100

    two task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,51 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+
+
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+
 console.log(galleryItems);
+
+const container = document.querySelector(".gallery")
+
+
+
+container.insertAdjacentHTML("beforeend", galleryList(galleryItems));
+container.addEventListener("click", handleClick);
+container.style.listStyle = "none";
+
+
+
+function galleryList(arr) {
+    return arr.map(({ preview, original, description }) =>
+        `
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}">
+    </a>
+    </li>
+    `
+    ).join("");
+};
+
+
+
+function handleClick(event) {
+
+    event.preventDefault()
+
+    if (event.target === event.curruntTarget) {
+        return gallery.next();
+    }
+
+    let gallery = new SimpleLightbox('.gallery__item a', { captionsData: "alt", captionDelay: 250, overlayOpacity: 0.5});
+    gallery.on('show.simplelightbox', function () {
+        `
+        <img class="gallery__image " src="${galleryItems.original}" alt="${galleryItems.description}">
+        `
+    });
+}
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import Player from '@vimeo/player';
+import { values } from '@vimeo/player';
+import throttle from 'lodash.throttle';
+// import { values } from '@vimeo/player';
 
+
+
+const vimeoFrame = document.querySelector("#vimeo-player");
+
+const player = new Player(vimeoFrame)
+
+const playFrame = throttle(function(data) {
+        localStorage.setItem("videoplayer-current-time", data.seconds)    
+}, 1000)
+
+player.on("timeupdate", playFrame)
+
+const currentTime = localStorage.getItem("videoplayer-current-time")
+player.setCurrentTime(currentTime);
