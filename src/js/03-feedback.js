b50084e256acd91226787a320874312eb93a163d
VOvaWAsp <127133873+VOvaWAsp@users.noreply.github.com>
2023-11-21 21:20:10 +0100

    maybe final

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,23 @@
+const feedbackForm = document.querySelector(".feedback-form");
 
+feedbackForm.addEventListener("input", (event) => {
+        
+     let valueEmail = event.currentTarget.email.value;
+     localStorage.setItem("feedback-form-state", valueEmail);
+
+     let valueMessage = event.currentTarget.message.value;
+     localStorage.setItem("feedback-form-message", valueMessage);
+
+     //  localStorage.getItem("feedback-form-state")
+ });
+
+ feedbackForm.email.value = localStorage.getItem("feedback-form-state");
+    feedbackForm.message.value = localStorage.getItem("feedback-form-message");
+    
+const handleSubmit = (event) => {
+    event.preventDefault();
+    feedbackForm.reset();
+    localStorage.removeItem("feedback-form-state")
+    localStorage.removeItem("feedback-form-message")
+}
+feedbackForm.addEventListener("submit", handleSubmit);
